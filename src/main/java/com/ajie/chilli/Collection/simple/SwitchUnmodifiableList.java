7ad4ad4124accae262c3dc34d24c4ed4639afa97
Mitnick5194 <qq519441630@gmail.com>
2018-09-09 15:10:44 +0800

    修改只读列表

--- src/main/java/com/ajie/chilli/Collection/simple/SwitchUnmodifiableList.java
+++ src/main/java/com/ajie/chilli/Collection/simple/SwitchUnmodifiableList.java
@@ -62,38 +62,39 @@ public class SwitchUnmodifiableList<E> extends ArrayList<E> {
 		unmodifiable = true;
 	}
 
-	public void checkModifiable() {
+	public void assertModifiable() {
 		if (unmodifiable)
-			throw new UnsupportedOperationException("列表只读");
+			throw new UnsupportedOperationException(
+					"hei man , what are you fucking doing , i am readonly you know");
 	}
 
 	@Override
 	public boolean add(E e) {
-		checkModifiable();
+		assertModifiable();
 		return super.add(e);
 	}
 
 	@Override
 	public void add(int index, E element) {
-		checkModifiable();
+		assertModifiable();
 		super.add(index, element);
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends E> c) {
-		checkModifiable();
+		assertModifiable();
 		return super.addAll(c);
 	}
 
 	@Override
 	public boolean addAll(int index, Collection<? extends E> c) {
-		checkModifiable();
+		assertModifiable();
 		return super.addAll(index, c);
 	}
 
 	@Override
 	public E set(int index, E element) {
-		checkModifiable();
+		assertModifiable();
 		return super.set(index, element);
 	}
 
